f2557f0c6252cd4c4ce0f0afaa3dd7b26277edf3
Simon <s.labzoff@yandex.ru>
2023-05-09 11:48:40 +0300

    refactoring

--- src/main.py
+++ src/main.py
@@ -1,35 +1,36 @@
 #!/usr/bin/python3
-# Description: The Python code below will search selenium in Google.
 
 import os
+import trio
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.bidi.console import Console
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.log import Log
 
- 
-options = Options()
-options.add_argument("--incognito")
-options.page_load_strategy = 'eager'
-driver = webdriver.Chrome(options=options, executable_path=os.path.join(os.path.dirname(__file__), os.pardir, 'bin', 'chromedriver'))
-print(driver.execute_cdp_cmd("Network.setBlockedURLs", {"urls": [
-    "*.css",
-    "*.png",
-    "*.jpg", 
-    "*.webp",
-    "*.woff2",
-    "*gstatic*",
-    "*uviewer*",
-    "*youtube*"
+if __name__ == "__main__":
+    options = Options()
+    options.add_argument('--incognito')
+    options.page_load_strategy = 'eager'
+    driver = webdriver.Chrome(options=options, executable_path=os.path.join(
+        os.path.dirname(__file__), os.pardir, 'bin', 'chromedriver'))
+    print(driver.execute_cdp_cmd('Network.setBlockedURLs', {'urls': [
+        '*.css',
+        '*.png',
+        '*.jpg',
+        '*.webp',
+        '*.woff2',
+        '*gstatic*',
+        '*uviewer*',
+        '*youtube*'
     ]}))
 
-print(driver.execute_cdp_cmd('Network.enable', {}))
+    driver.get('https://www.google.com')
+    search_box = driver.find_element(by=By.NAME, value='q')
+    search_box.send_keys('selenium')
+    search_box.send_keys(Keys.RETURN)
+    driver.execute_cdp_cmd('Network.enable', {})
 
-driver.get("https://www.google.com")
-search_box = driver.find_element(by=By.NAME, value='q')  # Find search input box.
-search_box.send_keys('selenium')               # Type in selenium.
-search_box.send_keys(Keys.RETURN)              # Press ENTER.
-
-input()
-# Close.
-#driver.close()
+    input()
+    driver.quit()
