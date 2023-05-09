6be9ee1a05b6bda80b3d2050d9ffa4fcac2a6681
Simon <s.labzoff@yandex.ru>
2023-05-09 16:44:03 +0300

    args

---
+++ scripts/test_run.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+BLACK_LIST='[ "*.css", "*.png", "*.ico", "*.jpg", "*.webp", "*.woff2", "*gstatic*", "*uviewer*", "*youtube*", "data:image*" ]'
+TARGET='https://www.google.com'
+MAIN_DIR="$SCRIPT_DIR/../src" 
+MAIN=$MAIN_DIR/main.py
+
+# python -m poetry run -C "$MAIN_DIR" python "$MAIN" "$TARGET" "$BLACK_LIST"
+python "$MAIN" --target "$TARGET" --black-list "$BLACK_LIST"
--- src/main.py
+++ src/main.py
@@ -1,64 +1,67 @@
 #!/usr/bin/python3
-
+import sys
 import os
 import trio
+import json
+import argparse
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.bidi.console import Console
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.log import Log
+from selenium.webdriver.chrome.service import Service
+
 from functools import partial
 
-BLACK_LIST = [
-    '*.css',
-    '*.png',
-    '*.ico',
-    '*.jpg',
-    '*.webp',
-    '*.woff2',
-    '*gstatic*',
-    '*uviewer*',
-    '*youtube*',
-    'data:image*'
-]
+TEST_RUN=False
 
-
-def test_code(driver):
-    driver.get('https://www.google.com')
-    search_box = driver.find_element(by=By.NAME, value='q')
-    search_box.send_keys('selenium')
-    search_box.send_keys(Keys.RETURN)
-
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--black-list')
+    parser.add_argument('--target')
+    args = parser.parse_args()
+    return json.loads(args.black_list), args.target
 
 async def start_listening(counter, listener):
     async for event in listener:
         counter += 1
-        print(counter, event.response.url[:80])
+        print(counter, 'receive:', event.response.url[:80])
 
-
-async def run():
-    options = Options()
-    options.add_argument('--incognito')
-    options.page_load_strategy = 'eager'
-    driver = webdriver.Chrome(options=options, executable_path=os.path.join(
-        os.path.dirname(__file__), os.pardir, 'bin', 'chromedriver'))
-
-    print(driver.execute_cdp_cmd(
-        'Network.setBlockedURLs', {'urls': BLACK_LIST}))
-    driver.execute_cdp_cmd('Network.enable', {})
-
+async def test_run(driver, target):
     async with driver.bidi_connection() as connection:
         session, devtools = connection.session, connection.devtools
         await session.execute(devtools.network.enable())
         counter = 0
         listener = session.listen(devtools.network.ResponseReceived)
         async with trio.open_nursery() as nursery:
-            # start_listening blocks, so we run it in another coroutine
             nursery.start_soon(partial(start_listening, counter), listener)
-            test_code(driver)
+            driver.get(target)
+
+def run():
+    options = Options()
+    options.add_argument('--incognito')
+    options.add_argument('headless')
+    options.page_load_strategy = 'eager'
+    chromedriver_path = os.path.join(os.path.dirname(
+        __file__), os.pardir, 'bin', 'chromedriver')
+    service = Service(executable_path=chromedriver_path)
+    driver = webdriver.Chrome(options=options, service=service)
+
+    black_list, target = get_args()
+    print(black_list, target)
+
+    driver.execute_cdp_cmd(
+        'Network.setBlockedURLs', {'urls': black_list})
+    driver.execute_cdp_cmd('Network.enable', {})
+    
+    if TEST_RUN:
+        trio.run(partial(test_run, driver, target))
+    else:
+        driver.get(target)
+
+    driver.quit()
 
 if __name__ == "__main__":
-    trio.run(run)
-    input()
-    driver.quit()
+    run()
