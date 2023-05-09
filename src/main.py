32e97ea146093ad46c9af76dea42954889cffa04
Simon <s.labzoff@yandex.ru>
2023-05-09 12:36:43 +0300

    fixes

--- src/main.py
+++ src/main.py
@@ -8,10 +8,34 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.bidi.console import Console
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.log import Log
+from functools import partial
 
-async def start_listening(listener):
+BLACK_LIST = [
+    '*.css',
+    '*.png',
+    '*.ico',
+    '*.jpg',
+    '*.webp',
+    '*.woff2',
+    '*gstatic*',
+    '*uviewer*',
+    '*youtube*',
+    'data:image*'
+]
+
+
+def test_code(driver):
+    driver.get('https://www.google.com')
+    search_box = driver.find_element(by=By.NAME, value='q')
+    search_box.send_keys('selenium')
+    search_box.send_keys(Keys.RETURN)
+
+
+async def start_listening(counter, listener):
     async for event in listener:
-        print(event.response.url[:80])
+        counter += 1
+        print(counter, event.response.url[:80])
+
 
 async def run():
     options = Options()
@@ -20,30 +44,19 @@ async def run():
     driver = webdriver.Chrome(options=options, executable_path=os.path.join(
         os.path.dirname(__file__), os.pardir, 'bin', 'chromedriver'))
 
+    print(driver.execute_cdp_cmd(
+        'Network.setBlockedURLs', {'urls': BLACK_LIST}))
+    driver.execute_cdp_cmd('Network.enable', {})
+
     async with driver.bidi_connection() as connection:
         session, devtools = connection.session, connection.devtools
         await session.execute(devtools.network.enable())
-        
+        counter = 0
         listener = session.listen(devtools.network.ResponseReceived)
         async with trio.open_nursery() as nursery:
-            nursery.start_soon(start_listening, listener) # start_listening blocks, so we run it in another coroutine
-            print(driver.execute_cdp_cmd('Network.setBlockedURLs', {'urls': [
-                '*.css',
-                '*.png',
-                '*.jpg',
-                '*.webp',
-                '*.woff2',
-                '*gstatic*',
-                '*uviewer*',
-                '*youtube*'
-            ]}))
-
-            print(driver.execute_cdp_cmd('Network.enable', {}))
-
-            driver.get('https://www.google.com')
-            search_box = driver.find_element(by=By.NAME, value='q')
-            search_box.send_keys('selenium')
-            search_box.send_keys(Keys.RETURN)
+            # start_listening blocks, so we run it in another coroutine
+            nursery.start_soon(partial(start_listening, counter), listener)
+            test_code(driver)
 
 if __name__ == "__main__":
     trio.run(run)
